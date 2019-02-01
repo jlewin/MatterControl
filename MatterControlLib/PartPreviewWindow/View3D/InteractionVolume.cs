a609ad96cf4e64df97160db0195560f9713814b7
John Lewin <lewin76@gmail.com>
2019-02-01 15:09:30 -0800

    Move AddTransformSnapshot to InteractiveScene

--- MatterControlLib/PartPreviewWindow/View3D/InteractionVolume.cs
+++ MatterControlLib/PartPreviewWindow/View3D/InteractionVolume.cs
@@ -189,8 +189,6 @@ namespace MatterHackers.MeshVisualizer
 
 		GuiWidget GuiSurface { get; }
 
-		void AddTransformSnapshot(Matrix4X4 originalTransform);
-
 		List<InteractionVolume> InteractionVolumes { get; }
 		double SnapGridDistance { get; }
 	}
