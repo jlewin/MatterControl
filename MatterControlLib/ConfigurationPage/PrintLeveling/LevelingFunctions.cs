d4fe2210a82afce16617f985c666c4dd8f9144bc
John Lewin <lewin76@gmail.com>
2019-02-17 09:54:22 -0800

    Replace AutoFeedback probe pages with single visual page

--- MatterControlLib/ConfigurationPage/PrintLeveling/LevelingFunctions.cs
+++ MatterControlLib/ConfigurationPage/PrintLeveling/LevelingFunctions.cs
@@ -119,7 +119,7 @@ namespace MatterHackers.MatterControl.ConfigurationPage.PrintLeveling
 
 		public List<Vector3> SampledPositions { get; }
 
-		private List<LevelingTriangle> Regions { get; } = new List<LevelingTriangle>();
+		public List<LevelingTriangle> Regions { get; } = new List<LevelingTriangle>();
 
 		public string ApplyLeveling(string lineBeingSent, Vector3 currentDestination)
 		{
