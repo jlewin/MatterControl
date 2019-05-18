3411b5c7f2955ac049d1530bd294c41f24d54446
John Lewin <lewin76@gmail.com>
2019-05-18 13:19:46 -0700

    Update path controls

--- MatterControlLib/DesignTools/EditorTools/RotateControls/PathControl.cs
+++ MatterControlLib/DesignTools/EditorTools/RotateControls/PathControl.cs
@@ -37,11 +37,13 @@ using MatterHackers.DataConverters3D;
 using MatterHackers.MatterControl;
 using MatterHackers.MatterControl.DesignTools;
 using MatterHackers.MeshVisualizer;
+using MatterHackers.RayTracer;
+using MatterHackers.RenderOpenGl.OpenGl;
 using MatterHackers.VectorMath;
 
 namespace MatterHackers.Plugins.EditorTools
 {
-	public class PathControl : IInteractionElement
+	public class PathControl : IGLInteractionElement
 	{
 		private IInteractionVolumeContext interactionContext;
 
@@ -54,6 +56,7 @@ namespace MatterHackers.Plugins.EditorTools
 		private List<PointWidget> targets = new List<PointWidget>();
 
 		private bool controlsRegistered = false;
+		private IEnumerable<VertexData> activePoints;
 
 		public PathControl(IInteractionVolumeContext context)
 		{
@@ -65,37 +68,101 @@ namespace MatterHackers.Plugins.EditorTools
 
 		public string Name => "Path Control";
 
+		public bool DrawOnTop => false;
+
 		public void CancelOperation()
 		{
-			var a = 1;
+		}
+
+		public void DrawGlContent(DrawGlContentEventArgs e)
+		{
+			//// Draw grid background with active BedColor
+			//GL.Disable(EnableCap.Texture2D);
+			//GL.Disable(EnableCap.Blend);
+
+			GL.Begin(BeginMode.Lines);
+			{
+				GL.Color4(theme.PrimaryAccentColor);
+
+				bool isStart = true;
+
+				Vector3 last = Vector3.Zero;
+				Vector3 first = Vector3.Zero;
+
+				foreach (var point in activePoints)
+				{
+					if (isStart
+						|| point.IsMoveTo)
+					{
+						last = new Vector3(point.position);
+						first = last;
+						isStart = false;
+					}
+					else if (point.IsVertex)
+					{
+						GL.Vertex3(last);
+						GL.Vertex3(last = new Vector3(point.position));
+					}
+					else if (point.IsClose)
+					{
+						GL.Vertex3(last);
+						GL.Vertex3(first);
+					}
+				}
+			}
+			GL.End();
+		}
+
+		public void LostFocus()
+		{
+			this.Reset();
+		}
+
+		private void Reset()
+		{
+			// Clear and close selection targets
+			foreach (var widget in targets)
+			{
+				widget.Close();
+			}
+
+			targets.Clear();
+
+			lastItem = null;
 		}
 
 		public void SetPosition(IObject3D selectedItem)
 		{
 			if (selectedItem != lastItem)
 			{
+				this.Reset();
+
 				lastItem = selectedItem;
-
-				foreach (var widget in targets)
-				{
-					widget.Close();
-				}
-
-				targets.Clear();
 
 				if (selectedItem is PathObject3D pathObject)
 				{
-					foreach (var v in pathObject.VertexSource.Vertices())
+					var vertexStorage = pathObject.VertexSource as VertexStorage;
+
+					activePoints = vertexStorage.Vertices();
+
+					//foreach (var v in activePoints.Where(p => p.command != ShapePath.FlagsAndCommand.FlagNone && !p.IsClose))
+
+					for (var i = 0; i < vertexStorage.Count; i++)
 					{
-						var widget = new PointWidget(world, v);
-						widget.Click += (s, e) =>
+						var command = vertexStorage.vertex(i, out double x, out double y);
+
+						if (ShapePath.is_vertex(command))
 						{
-							Console.WriteLine("Hello World!");
-						};
+							var widget = new VertexPointWidget(interactionContext, vertexStorage, new Vector3(x, y, 0), command, i);
+							widget.Click += (s, e) =>
+							{
+								Console.WriteLine("Hello Worl2d!");
+							};
 
-						targets.Add(widget);
+							targets.Add(widget);
 
-						interactionContext.GuiSurface.AddChild(widget);
+							interactionContext.GuiSurface.AddChild(widget);
+						}
 					}
 				}
 			}
@@ -106,24 +173,53 @@ namespace MatterHackers.Plugins.EditorTools
 			}
 		}
 
+		private class VertexPointWidget : PointWidget
+		{
+			private ShapePath.FlagsAndCommand command;
+			private int index;
+			private VertexStorage vertexStorage;
+
+			public VertexPointWidget(IInteractionVolumeContext interactionContext, VertexStorage vertexStorage, Vector3 point, ShapePath.FlagsAndCommand flagsandCommand, int index)
+				: base(interactionContext, point)
+			{
+				this.command = flagsandCommand;
+				this.index = index;
+				this.vertexStorage = vertexStorage;
+			}
+
+			protected override void OnDragTo(IntersectInfo info)
+			{
+				this.Point = info.HitPosition;
+				vertexStorage.modify_vertex(index, info.HitPosition.X, info.HitPosition.Y);
+				this.Invalidate();
+				base.OnDragTo(info);
+			}
+		}
+
 		private class PointWidget : GuiWidget
 		{
-			private VertexData vertexData;
-			private Vector3 point;
 			private WorldView world;
+			private GuiWidget guiSurface;
 			private bool mouseInBounds;
 			private GuiWidget systemWindow;
+			private bool mouseDownOnWidget;
+			private IInteractionVolumeContext interactionContext;
+			private static PlaneShape bedPlane = new PlaneShape(Vector3.UnitZ, 0, null);
 
-			public PointWidget(WorldView world, VertexData vertexData)
+			public PointWidget(IInteractionVolumeContext interactionContext, Vector3 point)
 			{
-				this.vertexData = vertexData;
-				this.point = new Vector3(vertexData.position);
-				this.world = world;
-				HAnchor = HAnchor.Absolute;
-				VAnchor = VAnchor.Absolute;
-				Width = 12;
-				Height = 12;
+				this.interactionContext = interactionContext;
+				this.HAnchor = HAnchor.Absolute;
+				this.VAnchor = VAnchor.Absolute;
+				this.Width = 12;
+				this.Height = 12;
+				this.Point = point;
+
+				world = interactionContext.World;
+				guiSurface = interactionContext.GuiSurface;
 			}
+
+			protected Vector3 Point { get; set; }
 
 			public override void OnLoad(EventArgs args)
 			{
@@ -143,6 +239,41 @@ namespace MatterHackers.Plugins.EditorTools
 				}
 
 				base.OnClosed(e);
+			}
+
+			public override void OnMouseDown(MouseEventArgs mouseEvent)
+			{
+				mouseDownOnWidget = mouseEvent.Button == MouseButtons.Left && this.PositionWithinLocalBounds(mouseEvent.Position);
+				base.OnMouseDown(mouseEvent);
+			}
+
+			public override void OnMouseUp(MouseEventArgs mouseEvent)
+			{
+				mouseDownOnWidget = false;
+				base.OnMouseUp(mouseEvent);
+			}
+
+			public override void OnMouseMove(MouseEventArgs mouseEvent)
+			{
+				// Drag item
+				if (mouseDownOnWidget)
+				{
+					var localMousePosition = mouseEvent.Position;
+
+					Vector2 meshViewerWidgetScreenPosition = this.TransformToParentSpace(guiSurface, localMousePosition);
+					Ray ray = world.GetRayForLocalBounds(meshViewerWidgetScreenPosition);
+
+					if (bedPlane.GetClosestIntersection(ray) is IntersectInfo info)
+					{
+						this.OnDragTo(info);
+					}
+				}
+
+				base.OnMouseMove(mouseEvent);
+			}
+
+			protected virtual void OnDragTo(IntersectInfo info)
+			{
 			}
 
 			public override void OnMouseEnterBounds(MouseEventArgs mouseEvent)
@@ -165,7 +296,7 @@ namespace MatterHackers.Plugins.EditorTools
 				if (mouseInBounds)
 				{
 					var position = this.TransformToScreenSpace(LocalBounds.Center);
-					e.Graphics2D.Circle(position, 7, Color.Blue.WithAlpha(80));
+					e.Graphics2D.Circle(position, 9, Color.Blue.WithAlpha(80));
 				}
 			}
 
@@ -177,7 +308,7 @@ namespace MatterHackers.Plugins.EditorTools
 
 			public void UpdatePosition()
 			{
-				this.Position = world.GetScreenPosition(point);
+				this.Position = world.GetScreenPosition(Point) - new Vector2(this.LocalBounds.Width / 2, this.LocalBounds.Height / 2);
 			}
 		}
 	}
