fa71f637310c79b16082c25bf1546f7cc3e566e7
John Lewin <lewin76@gmail.com>
2026-01-09 23:41:12 -0800

    Add missing file

---
+++ MatterControlLib/PrinterCommunication/ConnectionProcessing.cs
@@ -0,0 +1,14 @@
+﻿using MatterHackers.MatterControl.PrinterCommunication;
+using MatterHackers.MatterControl.PrinterCommunication.Io;
+
+namespace MatterControlLib.PrinterCommunication
+{
+	public record ConnectionProcessing
+	{
+		public GCodeStream StreamPipeline { get; set; }
+		public IHeatableTarget HeatableTarget { get; set; }
+		public IPausableTarget PausableTarget { get; set; }
+		public ILevelingTarget LevelingTarget { get; set; }
+		public IQueuedCommands QueuedCommands { get; set; }
+	}
+}
